8377b148bdac88e44380c8b5533d750431a8f876
David Hilscher <davidhilscher@gmail.com>
2017-11-21 11:17:05 -0800

    Added redux to project.

--- js/App.js
+++ js/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import About from "./scenes/About";
-import { NavigationProvider, StackNavigation } from "@expo/ex-navigation";
+import {
+  NavigationContext,
+  NavigationProvider,
+  StackNavigation
+} from "@expo/ex-navigation";
 import Router from "./navigation/routes";
+import Store from "./redux/store";
+import { Provider } from "react-redux";
 
+const navigationContext = new NavigationContext({
+  router: Router,
+  store: Store
+});
 export default class App extends Component {
   render() {
     return (
-      <NavigationProvider router={Router}>
-        <StackNavigation initialRoute={Router.getRoute("about")} />
-      </NavigationProvider>
+      <Provider store={Store}>
+        <NavigationProvider context={navigationContext} router={Router}>
+          <StackNavigation initialRoute={Router.getRoute("about")} />
+        </NavigationProvider>
+      </Provider>
     );
   }
 }
