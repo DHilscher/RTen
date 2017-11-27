2f54999305467265d3eb9133b31d60780d05aefe
David Hilscher <davidhilscher@gmail.com>
2017-11-27 07:43:27 -0800

    Added styles to session scene.

--- js/scenes/Session/Session.js
+++ js/scenes/Session/Session.js
@@ -1,58 +1,72 @@
 import React from "react";
-import {
-  Text,
-  View,
-  TouchableHighlight,
-  Image,
-  Button,
-  Platform
-} from "react-native";
+import { Text, View, TouchableHighlight, Image, Platform } from "react-native";
 import PropTypes from "prop-types";
 import { goToSpeaker } from "../../lib/navigationHelpers";
 import Moment from "moment";
 import { createFave, deleteFave } from "../../config/models";
 import Icon from "react-native-vector-icons/Ionicons";
 import styles from "./styles";
+import { colors } from "../../config/styles";
+import LinearGradient from "react-native-linear-gradient";
 
 const Session = ({ data, speakerData, allFaves }) => {
-  console.log(speakerData);
-
   return (
-    <View>
-      <View key={data.session_id}>
-        {allFaves.indexOf(data.session_id) !== -1 ? (
-          <Icon
-            style={styles.Icon}
-            name={Platform.OS == "ios" ? "ios-heart" : "md-heart"}
-            size={14}
-          />
-        ) : null}
-        <Text>{data.location}</Text>
-        <Text>{data.title}</Text>
-        <Text>{Moment.unix(data.start_time).format("h:mm a")}</Text>
-        {speakerData ? (
+    <View style={styles.Container}>
+      {allFaves.indexOf(data.session_id) !== -1 ? (
+        <Icon
+          style={styles.Icon}
+          name={Platform.OS == "ios" ? "ios-heart" : "md-heart"}
+          size={14}
+        />
+      ) : null}
+      <View>
+        <Text style={styles.Location}>{data.location}</Text>
+        <Text style={styles.Title}>{data.title}</Text>
+        <Text style={styles.Time}>
+          {Moment.unix(data.start_time).format("h:mm a")}
+        </Text>
+        <Text style={styles.Description}>{data.description}</Text>
+      </View>
+      {speakerData ? (
+        <View>
+          <Text style={styles.Presented}>Presented by:</Text>
           <TouchableHighlight onPress={() => goToSpeaker(speakerData)}>
-            <View>
+            <View style={styles.SpeakerContainer}>
               <Image
-                style={{ height: 75, width: 75, borderRadius: 37.5 }}
+                style={styles.Image}
                 source={{ uri: `${speakerData.image}` }}
               />
-              <Text>{speakerData.name}</Text>
+              <Text style={styles.Name}>{speakerData.name}</Text>
             </View>
           </TouchableHighlight>
-        ) : null}
+        </View>
+      ) : null}
+      <View style={styles.Border} />
+      <View>
+        <LinearGradient
+          start={{ x: 0.9, y: 0 }}
+          end={{ x: 0.3, y: 1.0 }}
+          locations={[0, 1]}
+          colors={[colors.purple, colors.blue]}
+          style={styles.ButtonGradient}
+        >
+          {allFaves.indexOf(data.session_id) === -1 ? (
+            <Text
+              style={styles.Button}
+              onPress={() => createFave(data.session_id)}
+            >
+              Add to Faves
+            </Text>
+          ) : (
+            <Text
+              style={styles.Button}
+              onPress={() => deleteFave(data.session_id)}
+            >
+              Remove from Faves
+            </Text>
+          )}
+        </LinearGradient>
       </View>
-      {allFaves.indexOf(data.session_id) === -1 ? (
-        <Button
-          title="addToFaves"
-          onPress={() => createFave(data.session_id)}
-        />
-      ) : (
-        <Button
-          title="deleteFromFaves"
-          onPress={() => deleteFave(data.session_id)}
-        />
-      )}
     </View>
   );
 };
--- js/scenes/Session/styles.js
+++ js/scenes/Session/styles.js
@@ -3,38 +3,82 @@ import { StyleSheet } from "react-native";
 import { typography, colors } from "../../config/styles";
 
 export default StyleSheet.create({
-  Date: {
+  Time: {
     fontFamily: typography.fontMainReg,
-    fontSize: 12,
-    marginLeft: "3%",
+    fontSize: 14,
     marginTop: 5,
-    marginBottom: 5
+    color: colors.red
   },
   Title: {
-    fontFamily: typography.fontMainReg,
+    fontFamily: typography.fontMainLight,
     marginTop: 10,
-    fontSize: 14
+    fontSize: 20
   },
   Location: {
     fontFamily: typography.fontMainLight,
-    marginTop: 10,
-    fontSize: 10
+    marginTop: 12,
+    fontSize: 12,
+    color: colors.medGrey
   },
   SectionHeader: {
     backgroundColor: colors.lightGrey
   },
-  AnimatedContainer: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-    flexDirection: "row",
-    marginTop: 20
+  Container: {
+    width: "92%",
+    alignSelf: "center",
+    justifyContent: "center"
   },
   Icon: {
     position: "absolute",
-    right: 15,
-    top: 1,
+    right: 5,
+    top: 15,
     color: colors.red,
     zIndex: 9
+  },
+  Description: {
+    fontFamily: typography.fontMainReg,
+    marginTop: 12,
+    fontSize: 12,
+    color: colors.medGrey
+  },
+  Presented: {
+    fontFamily: typography.fontMainReg,
+    marginTop: 18,
+    fontSize: 12,
+    color: colors.medGrey
+  },
+  SpeakerContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  Border: {
+    height: 2,
+    backgroundColor: colors.lightGrey
+  },
+  Image: {
+    width: 40,
+    height: 40,
+    borderRadius: 20
+  },
+  Name: {
+    fontFamily: typography.fontMainReg,
+    fontSize: 12,
+    color: colors.medGrey,
+    marginLeft: "3%"
+  },
+  Button: {
+    fontFamily: typography.fontMainReg,
+    backgroundColor: "transparent",
+    color: "white",
+    alignSelf: "center",
+    fontSize: 12
+  },
+  ButtonGradient: {
+    padding: 8,
+    borderRadius: 50,
+    alignSelf: "center",
+    marginTop: 15
   }
 });
